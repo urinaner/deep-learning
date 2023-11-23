bd51cb31fa6d4bcf9ec8194a31840ac2af51d386
Junibro1 <junibro1@gmail.com>
2023-11-23 15:59:02 +0900

    d

--- make_dataset.py
+++ make_dataset.py
@@ -1,6 +1,12 @@
 import torch
+import numpy as np
 import os
+from PIL import Image 
 
-train_path = ".\dataset\train"
+train_path = "C:/Users/wnslg/Desktop/yj/deep-learning/dataset/train"
 
-test_path = ".\dataset\test"
+test_path = "C:/Users/wnslg/Desktop/yj/deep-learning/dataset/test"
+
+for labels in sorted(os.listdir(train_path)):
+    for imgs in sorted(os.listdir(train_path+'/'+labels)):
+        print(imgs)
